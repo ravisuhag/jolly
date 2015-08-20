094a90141f4c408948cca255f8c51d115d253ad2
Ravi Suhag <suhag.ravi@gmail.com>
2015-08-20 06:38:47 +0530

    Adding base manifest

--- config/manifest.js
+++ config/manifest.js
@@ -1,17 +1,37 @@
 'use strict';
 
-// Load Modules 
 var Confidence = require('confidence');
-
-
-var store = new Confidence.Store({
-  name: 'jolly'
-});
+var Config = require('./config');
 
 var criteria = {
-  env: process.env.ENVIRONMENT
+  env: process.env.NODE_ENV
 };
+
+var manifest = {
+  $meta: 'jolly app manifest document.',
+  server: {
+    debug: {
+      request: ['error']
+    },
+    connections: {
+      routes: {
+        security: true
+      }
+    }
+  },
+  connections: [{
+    port: Config.get('/port/web'),
+    labels: ['web']
+  }]
+};
+
+
+var store = new Confidence.Store(manifest);
+
 
 exports.get = function(key) {
   return store.get(key, criteria);
+};
+exports.meta = function(key) {
+  return store.meta(key, criteria);
 };

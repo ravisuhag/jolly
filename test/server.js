330e51a1bc03d1cac5b9aa5ae47d5183b19d3483
Ravi Suhag <suhag.ravi@gmail.com>
2015-08-20 08:45:52 +0530

    Adding server test

---
+++ test/server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Load modules
+var Hapi = require('hapi');
+var Code = require('code');
+var Lab = require('lab');
+var Composer = require('../index');
+
+// Test shortcuts
+var lab = exports.lab = Lab.script();
+var describe = lab.experiment;
+var expect = Code.expect;
+var it = lab.test;
+
+it('starts server and returns hapi server object', function(done) {
+
+  Composer(function(err, server) {
+
+    expect(err).to.not.exist();
+    expect(server).to.exist();
+    server.stop(done);
+
+  });
+
+});

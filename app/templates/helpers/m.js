2ed57cf1f92f1d3e91e756921ea47fa8dffae2f5
turtleDev <saravjeetamansingh@gmail.com>
2016-07-25 12:47:24 +0530

    fix rev-manifest errors

--- app/templates/helpers/m.js
+++ app/templates/helpers/m.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /* Manifest file reader for grabbing new static assets */
-var manifest = require('../../static/rev-manifest');
+var manifest = require('../../../.build/rev-manifest');
 
 module.exports = function m(filename) {
   return manifest[filename];
